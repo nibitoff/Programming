f637bf934d270a5b556da16ebcdef2243ebd868c
nibibee <72893410+nibibee@users.noreply.github.com>
2021-01-15 17:21:08 +0300

    Update Stick.java

--- Lab4/Stick.java
+++ Lab4/Stick.java
@@ -1,4 +1,7 @@
 public final class Stick {
+    /**
+    class that shows if stick is cracked
+    */
     //method that returns isCracked value
     public boolean isCracked(){
         return false;
