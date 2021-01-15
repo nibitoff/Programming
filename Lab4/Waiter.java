5ef5cb99df8ca93efd221533c9b860b2564e4ef6
sabitov <sabitov@users.noreply.github.com>
2021-01-15 16:53:51 +0300

    Create Waiter.java

---
+++ Lab4/Waiter.java
@@ -0,0 +1,64 @@
+public class Waiter extends FairyCharacter implements ForWaiter {
+    Money coins = new Money();
+    Food meals = new Food();
+
+    //method that outputs message and food's cost
+    public void take() {
+            System.out.println("Пожалуйста, заплатите за заказ " + meals.getFoodCost() + ".");
+        }
+
+    //method that checks if waiter got necessary value of money
+    public void give(Waiter waiter) {
+            if (waiter.getCoins() == meals.getFoodCost()) {
+                //if true, outputs message and Food.getFood()
+                System.out.println("Держите Ваш заказ " + meals.getFood() + ".");
+            }
+        }
+
+    //getter for waiter's money
+    public int getCoins() {
+        return coins.getCount();
+    }
+
+    //setter for waiter's money
+    public void setCoins(int coins) {
+        this.coins.setCount(coins);
+    }
+    //methods methods below override abstract methods from FairyCharacter abstract class
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    @Override
+    public String getJob() {
+        return this.job;
+    }
+
+    @Override
+    public void setHunger(boolean hunger) {
+        this.hunger = hunger;
+    }
+
+    @Override
+    public boolean getHunger() {
+        return this.hunger;
+    }
+    @Override
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+
+}
+
