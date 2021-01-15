f7d0f93a65877021f9fa736bf11e122a94c17d60
nibibee <72893410+nibibee@users.noreply.github.com>
2021-01-15 17:11:14 +0300

    Update FreedomError.java

--- Lab4/FreedomError.java
+++ Lab4/FreedomError.java
@@ -1,4 +1,7 @@
 public class FreedomError extends Exception{
+    /**
+    Error that shows when villain is free
+    */
     public FreedomError(){}
     public FreedomError(String message) {
         super(message);
