e2dfad822b151d6ea6d5469c4148b924a70eb4b7
nibibee <72893410+nibibee@users.noreply.github.com>
2021-01-15 17:35:51 +0300

    Update MoneyException.java

--- Lab4/MoneyException.java
+++ Lab4/MoneyException.java
@@ -2,7 +2,6 @@ public class MoneyException extends RuntimeException{
         /**
         Error that shows when characters have negative value of money
         */
-        public MoneyException(){}
         public MoneyException(String message){
             super(message);
         }
