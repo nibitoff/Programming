c017cf0b7b64405d93a797c6dea3ecf747a8c58e
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:55:29 +0300

    Create Food.java

---
+++ Lab4/Food.java
@@ -0,0 +1,17 @@
+public final class Food {
+    /**
+    class which represents food
+    */
+    private final int foodCost = 1000;
+
+    //getter for food
+    public String getFood() {
+        return "Пирожки";
+        }
+    // getter for foodCost
+    public int getFoodCost() {
+        return foodCost;
+    }
+}
+
+
