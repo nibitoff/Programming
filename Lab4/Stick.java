6d6e849b665d51ecdbe50165bed34950c57377a5
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:56:08 +0300

    Create Stick.java

---
+++ Lab4/Stick.java
@@ -0,0 +1,9 @@
+public final class Stick {
+    /**
+    class that shows if stick is cracked
+    */
+    //method that returns isCracked value
+    public boolean isCracked(){
+        return false;
+    }
+}
