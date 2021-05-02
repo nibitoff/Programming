795ec758396fbaf5e9ddc432edf91aab6608b254
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:55:36 +0300

    Create ForVillain.java

---
+++ Lab4/ForVillain.java
@@ -0,0 +1,22 @@
+public interface ForVillain {
+    /**
+    interface for Villain class
+    */
+    void pay(Villain villain, Waiter waiter);
+    void eat();
+    void imagine();
+    void say();
+    void broke(Stick stick);
+    void listen(Sound sound);
+    void setName(String name);
+    String getName();
+    void setJob(String job);
+    String getJob();
+    void setHunger(boolean hunger);
+    boolean getHunger();
+    void setMood(String mood);
+    void haveThirst();
+    void setThirst(boolean thirst);
+    boolean getThirst();
+
+}
