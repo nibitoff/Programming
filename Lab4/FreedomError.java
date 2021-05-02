7a6c8a5a08357309a7d3126fec2b948e9d265bb6
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:55:41 +0300

    Create FreedomError.java

---
+++ Lab4/FreedomError.java
@@ -0,0 +1,9 @@
+public class FreedomError extends Exception{
+    /**
+    Error that shows when villain is free
+    */
+    public FreedomError(String message) {
+        super(message);
+    }
+
+}
