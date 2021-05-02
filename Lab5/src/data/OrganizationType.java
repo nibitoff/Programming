4a7b842a1ac4926f2c489b590ee861e83014b693
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:49:15 +0300

    Create OrganizationType.java

---
+++ Lab5/src/data/OrganizationType.java
@@ -0,0 +1,13 @@
+package data;
+
+/**
+ * @author Sabitov Danil
+ * @version 1.0
+ * Enum class for describing field OrganizationType of element
+ */
+
+public enum OrganizationType {
+    TRUST,
+    PRIVATE_LIMITED_COMPANY,
+    OPEN_JOINT_STOCK_COMPANY;
+}
