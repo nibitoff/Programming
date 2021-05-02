cd47114e5630ce96f4be1f8aa3093fe0fa9467f1
sabitov <sabitov@users.noreply.github.com>
2021-05-02 17:54:20 +0300

    Create ForWaiter.java

---
+++ Lab3/ForWaiter.java
@@ -0,0 +1,14 @@
+public interface ForWaiter {
+    void take();
+    void give();
+    void setName(String name);
+    String getName();
+    void setJob(String job);
+    String getJob();
+    void setPlace(String place);
+    String getPlace();
+    void setHunger(boolean hunger);
+    boolean getHunger();
+    void setMood(String mood);
+    String getMood();
+}
