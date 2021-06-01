24d18f76f7c6f451e5d2605da94f634c46c3d7ae
sabitov <sabitov@users.noreply.github.com>
2021-06-02 02:09:23 +0300

    Create Filter_greater_than_annual_turnover.java

---
+++ Lab6/client/commands/Filter_greater_than_annual_turnover.java
@@ -0,0 +1,17 @@
+/*
+package commands;
+
+import data.Organization;
+
+public class Filter_greater_than_annual_turnover {
+    long annual = Long.parseLong(orgAnnual);
+    Organization newOrg = new Organization(0, null, annual, "0",
+            null, null, null, null);
+    ClientTCP sender = new ClientTCP();
+        sender.setCommand("remove_greater");
+        sender.setOrg(newOrg);
+        sender.sending(sender);
+}
+
+
+ */
