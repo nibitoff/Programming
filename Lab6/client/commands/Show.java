c85661ede10d559f0db52b6901f6a98b337553a1
sabitov <sabitov@users.noreply.github.com>
2021-06-02 02:09:42 +0300

    Update Show.java

--- Lab6/client/commands/Show.java
+++ Lab6/client/commands/Show.java
@@ -4,18 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.Organization;
 
+import java.io.IOException;
+
 public class Show {
-    public static void show() throws JsonProcessingException, InterruptedException {
+    public void show() throws  InterruptedException {
 
-        Sender sender = new Sender();
+        ClientTCP sender = new ClientTCP();
         sender.setCommand("show");
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        //Converting the Object to JSONString
-        String jsonString = mapper.writeValueAsString(sender);
-        String toServer = jsonString.replaceAll("[\\\t|\\\n|\\\r]"," ");
-        System.out.println(toServer);
         sender.sending(sender);
 
     }
