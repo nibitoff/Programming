aa3cfac93210373ba262c4ca548d8653f0b22478
sabitov <sabitov@users.noreply.github.com>
2021-06-02 02:09:25 +0300

    Update Help.java

--- Lab6/client/commands/Help.java
+++ Lab6/client/commands/Help.java
@@ -3,6 +3,14 @@ package commands;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import data.Organization;
 
-public class Help {
+import java.io.IOException;
 
+public class Help {
+    public void help() throws InterruptedException {
+
+        ClientTCP sender = new ClientTCP();
+        sender.setCommand("help");
+        sender.sending(sender);
+
+    }
 }
