15698cf92617f912f320096cec8e5e450c6e0698
sabitov <sabitov@users.noreply.github.com>
2021-06-02 22:06:38 +0300

    Update Execute_script.java

--- Lab6/client/commands/Execute_script.java
+++ Lab6/client/commands/Execute_script.java
@@ -1,4 +1,13 @@
 package commands;
 
+import data.Organization;
+
 public class Execute_script {
+    public void execute_script(String filePath) throws InterruptedException {
+        ClientTCP sender = new ClientTCP();
+        sender.setCommand("execute_script");
+        sender.setFilePath(filePath);
+        sender.sending(sender);
+    }
+
 }
