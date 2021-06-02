cb418bdc925d97d65c7ef0b48d3b899f709d3349
sabitov <sabitov@users.noreply.github.com>
2021-06-02 22:06:42 +0300

    Update Filter_greater_than_annual_turnover.java

--- Lab6/client/commands/Filter_greater_than_annual_turnover.java
+++ Lab6/client/commands/Filter_greater_than_annual_turnover.java
@@ -1,17 +1,16 @@
-/*
 package commands;
 
 import data.Organization;
 
-public class Filter_greater_than_annual_turnover {
-    long annual = Long.parseLong(orgAnnual);
-    Organization newOrg = new Organization(0, null, annual, "0",
-            null, null, null, null);
-    ClientTCP sender = new ClientTCP();
-        sender.setCommand("remove_greater");
+public class Filter_greater_than_annual_turnover{
+    public void filter_annual_turnover(String orgAnnual) throws InterruptedException {
+        long annual = Long.parseLong(orgAnnual);
+        Organization newOrg = new Organization(0, null, annual, "0",
+                null, null, null, null);
+        ClientTCP sender = new ClientTCP();
+        sender.setCommand("filter_greater_than_annual_turnover");
         sender.setOrg(newOrg);
         sender.sending(sender);
+    }
 }
 
-
- */
