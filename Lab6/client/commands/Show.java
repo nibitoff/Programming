a7b226ded9a44d562691a500ce268951383596d9
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:35:16 +0300

    Update Show.java

--- Lab6/client/commands/Show.java
+++ Lab6/client/commands/Show.java
@@ -5,10 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import data.Organization;
 
 import java.io.IOException;
-
+/**
+ * Class for command "show"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Show {
+    /**
+     * Method for realizing this command
+     */
     public void show() throws  InterruptedException {
-
         ClientTCP sender = new ClientTCP();
         sender.setCommand("show");
         sender.sending(sender);
