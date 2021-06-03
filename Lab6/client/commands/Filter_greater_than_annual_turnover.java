978954dc0e48b9f3be0179a0c72c198e198befc7
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:34:47 +0300

    Update Filter_greater_than_annual_turnover.java

--- Lab6/client/commands/Filter_greater_than_annual_turnover.java
+++ Lab6/client/commands/Filter_greater_than_annual_turnover.java
@@ -2,8 +2,19 @@ package commands;
 
 import data.Organization;
 
+import java.io.IOException;
+
+/**
+ * Class for command "filter_annual_turnover"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Filter_greater_than_annual_turnover{
-    public void filter_annual_turnover(String orgAnnual) throws InterruptedException {
+    /**
+     * Method for realizing this command
+     * @param orgAnnual - string representation of organization's annual turnover
+     */
+    public void filter_annual_turnover(String orgAnnual) throws InterruptedException{
         long annual = Long.parseLong(orgAnnual);
         Organization newOrg = new Organization(0, null, annual, "0",
                 null, null, null, null);
