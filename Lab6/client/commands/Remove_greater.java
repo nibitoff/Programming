43ee743baab6ab7f0a9b898785588ab8520b3518
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:35:07 +0300

    Update Remove_greater.java

--- Lab6/client/commands/Remove_greater.java
+++ Lab6/client/commands/Remove_greater.java
@@ -1,8 +1,16 @@
 package commands;
 
 import data.Organization;
-
+/**
+ * Class for command "remove_annual"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Remove_greater {
+    /**
+     * Method for realizing this command
+     * @param orgAnnual - string representation of organization's annual turnover
+     */
     public void remove_annual(String orgAnnual) throws  InterruptedException{
         long annual = Long.parseLong(orgAnnual);
         Organization newOrg = new Organization(0, null, annual, "0",
