f0c854e841da7f00df4fadeb64835d754368a24a
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:35:24 +0300

    Update ClientMain.java

--- Lab6/client/helpers/ClientMain.java
+++ Lab6/client/helpers/ClientMain.java
@@ -9,13 +9,15 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 import commands.*;
+/**
+ * Main class for starting a program
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class ClientMain {
-
-
     public static void main(String[] args) throws InterruptedException {
         try {
             System.out.println("Сlient program started!");
-            //Sender sender = new Sender();
         CommandManager commandManager = new CommandManager();
         commandManager.cmdMode();
        } catch(ArrayIndexOutOfBoundsException exception){
@@ -23,6 +25,4 @@ public class ClientMain {
             System.exit(1);
         }
     }
-
-
     }
