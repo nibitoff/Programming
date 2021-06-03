a67953c92aa5753e105234eff393a70b105895ed
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:34:54 +0300

    Update Info.java

--- Lab6/client/commands/Info.java
+++ Lab6/client/commands/Info.java
@@ -3,10 +3,16 @@ package commands;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.io.IOException;
-
+/**
+ * Class for command "info"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Info {
+    /**
+     * Method for realizing this command
+     */
     public void info() throws  InterruptedException {
-
         ClientTCP sender = new ClientTCP();
         sender.setCommand("info");
         sender.sending(sender);
