3b94740c31f0565e2eaa92d1edccb2bdfb154931
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:34:32 +0300

    Update Clear.java

--- Lab6/client/commands/Clear.java
+++ Lab6/client/commands/Clear.java
@@ -3,8 +3,15 @@ package commands;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.io.IOException;
-
+/**
+ * Class for command "clear"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Clear {
+    /**
+     * Method for realizing this command
+     */
     public void clear() throws  InterruptedException {
 
         ClientTCP sender = new ClientTCP();
