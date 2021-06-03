114dd3b5be6fd033998260866c2ae3238f360eae
sabitov <sabitov@users.noreply.github.com>
2021-06-03 15:35:03 +0300

    Update Remove_first.java

--- Lab6/client/commands/Remove_first.java
+++ Lab6/client/commands/Remove_first.java
@@ -1,8 +1,15 @@
 package commands;
 
 import data.Organization;
-
+/**
+ * Class for command "remove_first"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Remove_first {
+    /**
+     * Method for realizing this command
+     */
     public void remove_first() throws  InterruptedException{
         ClientTCP sender = new ClientTCP();
         sender.setCommand("remove_first");
