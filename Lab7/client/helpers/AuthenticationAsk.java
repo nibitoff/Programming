54d8c3b903860aefd6de1f78539c3ee63be6e0b4
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:29:37 +0300

    Create AuthenticationAsk.java

---
+++ Lab7/client/helpers/AuthenticationAsk.java
@@ -0,0 +1,97 @@
+package helpers;
+
+
+import exceptions.NotDeclaredLoginException;
+import exceptions.NotDeclaredPasswordException;
+
+import java.io.Console;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class AuthenticationAsk {
+    private Scanner scanner;
+    String pattern = "(?=\\S+$).{3,}";
+
+    public AuthenticationAsk(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String askLogin() {
+        String login;
+        while (true) {
+            try {
+                System.out.println("Enter your login:");
+                login = scanner.nextLine().trim();
+                if (login.equals("")) throw new NotDeclaredLoginException();
+                break;
+            } catch (NotDeclaredLoginException e) {
+                System.out.println("Login shouldn't be empty!");
+            }
+        }
+        return login;
+    }
+
+    public String askPassword() {
+        String password;
+        while (true) {
+            try {
+                System.out.println("Enter password:");
+                Console console = System.console();
+                if (console == null) {
+                    password = scanner.nextLine();
+                    password = encryptPassword(password);
+                } else {
+                    password = String.valueOf(console.readPassword());
+                    password = encryptPassword(password);
+                }
+                if (password.equals("")) throw new NotDeclaredLoginException();
+                if (!password.matches(pattern)) throw new NotDeclaredPasswordException();
+                break;
+            } catch (NotDeclaredLoginException e) {
+                System.out.println("Password shouldn't be empty!");
+            } catch (NotDeclaredPasswordException e) {
+                System.out.println("Password cannot contains spaces and it must be more than 3 characters!");
+            }
+        }
+        return password;
+    }
+
+    public boolean askQuestion(String question) {
+        String finalQuestion = question + " (yes/no):";
+        String answer;
+        while (true) {
+            try {
+                System.out.println(finalQuestion);
+                answer = scanner.nextLine().trim();
+                if (!answer.equals("yes") && !answer.equals("no")) throw new NotDeclaredLoginException();
+                if (answer.equals("no")){
+                    System.out.println("Please register new account!");
+                }
+                return answer.equals("yes");
+            } catch (NotDeclaredLoginException e) {
+                System.out.println("The answer must be either 'yes' or 'no'!");
+            }
+        }
+    }
+
+    public String encryptPassword(String password) {
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-384");
+            byte[] digest = messageDigest.digest(password.getBytes(StandardCharsets.UTF_8));
+            BigInteger numRepr = new BigInteger(1, digest);
+            String hashString = numRepr.toString(16);
+            while (hashString.length() < 32){
+                hashString += "0";
+            }
+            return hashString;
+        } catch (NoSuchElementException | NoSuchAlgorithmException e){
+            System.out.println("Something wrong! I can feel it.");
+        }
+        return null;
+    }
+
+}
