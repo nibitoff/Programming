638703e13fa170d5238daf4baee1f1f6db42db0f
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:29:07 +0300

    Create Remove_id.java

---
+++ Lab7/client/commands/Remove_id.java
@@ -0,0 +1,24 @@
+package commands;
+
+import data.Organization;
+/**
+ * Class for command "remove_id
+ * @author Sabitov Danil
+ * @version 1.0
+ */
+public class Remove_id {
+    /**
+     * Method for realizing this command
+     * @param orgId - string representation of organization's id
+     */
+    public void remove_id(String orgId) throws  InterruptedException{
+        int id = Integer.parseInt(orgId);
+        Organization newOrg = new Organization(id, null, null, "0",
+                null, null, null, null, null);
+        ClientTCP sender = new ClientTCP();
+        sender.setCommand("remove_by_id");
+        sender.setOrg(newOrg);
+        sender.sending(sender);
+
+    }
+}
