ff2da26300fabc416cb9ad25760c0d542d5f89e8
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:29:18 +0300

    Create Coordinates.java

---
+++ Lab7/client/data/Coordinates.java
@@ -0,0 +1,41 @@
+package data;
+
+/**
+ * @author Sabitov Danil
+ * @version 1.0
+ * Class for describing field Coordinates of element
+ */
+
+public class Coordinates {
+    private Double x; //Поле не может быть null
+    private Float y; //Поле не может быть null
+
+    public Coordinates(double x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+
+    /** method that prints coordinates in a string representation */
+    @Override
+    public String toString() {
+        return
+                x + " " + y ;
+}
+}
