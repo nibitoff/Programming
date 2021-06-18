02b707bc0299bb6411f48f41b724c67a85d0d398
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:30:15 +0300

    Create Server.java

---
+++ Lab7/server/Server.java
@@ -0,0 +1,28 @@
+
+import data.Organization;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static String myResult;
+
+    public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
+        try {
+            System.out.println("Program started working!");
+            CommandManager commandManager = new CommandManager(new CollectionChecker());
+            System.out.println("Server started!");
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Incorrect filepath was entered! Try again!");
+            System.exit(1);
+        }
+            Reciever reciever = new Reciever();
+            reciever.run();
+
+    }
+}
+
