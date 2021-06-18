114bc928b057341f30814f8554ccb88652af79a6
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:29:42 +0300

    Create User.java

---
+++ Lab7/client/helpers/User.java
@@ -0,0 +1,45 @@
+package helpers;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o instanceof User) {
+            User userObj = (User) o;
+            return login.equals(userObj.getLogin()) && password.equals(userObj.getPassword());
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return login + ","
+                + password;
+    }
+}
