62c3c61c929db83c050eca9b62c8d5ffdc4a4282
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-19 00:29:55 +0300

    Create DataChecker.java

---
+++ Lab7/server/data/DataChecker.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DataChecker {
+
+    public static boolean isValidDate(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+}
