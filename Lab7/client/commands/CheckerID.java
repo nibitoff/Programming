2c173d6575b65cbe56be0282b82c6afde787e302
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-20 18:25:35 +0300

    Update CheckerID.java

--- Lab7/client/commands/CheckerID.java
+++ Lab7/client/commands/CheckerID.java
@@ -1,8 +1,15 @@
 package commands;
 
 import data.Organization;
-
+/**
+ * Class for command "checkID"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class CheckerID {
+    /**
+     * Method for realizing this command
+     */
     public void checker(String orgId, String command) throws  InterruptedException{
         int id = Integer.parseInt(orgId);
         Organization newOrg = new Organization(id, command, null, "0",
