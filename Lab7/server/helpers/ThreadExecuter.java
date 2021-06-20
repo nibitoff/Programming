a6c1661298fb4deda8ca3ff97ea745b58a4cc6c7
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-20 18:26:06 +0300

    Update ThreadExecuter.java

--- Lab7/server/helpers/ThreadExecuter.java
+++ Lab7/server/helpers/ThreadExecuter.java
@@ -1,6 +1,11 @@
 package helpers;
 import java.util.concurrent.Callable;
 
+/**
+ * @author Sabitov Danil
+ * @version 1.0
+ * Class describing multi-thread execution of commands from client
+ */
 public class ThreadExecuter implements Callable<String> {
     Sender s;
     public ThreadExecuter(Sender s){
