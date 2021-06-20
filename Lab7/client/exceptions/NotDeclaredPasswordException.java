1b858c994f1ec5206e8459bef8c4c0767f78863f
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-20 18:25:47 +0300

    Update NotDeclaredPasswordException.java

--- Lab7/client/exceptions/NotDeclaredPasswordException.java
+++ Lab7/client/exceptions/NotDeclaredPasswordException.java
@@ -1,4 +1,8 @@
 package exceptions;
-
+/**
+ * @author Sabitov Danil
+ * @version 1.0
+ * Class for describing PasswordException
+ */
 public class NotDeclaredPasswordException extends Exception{
 }
