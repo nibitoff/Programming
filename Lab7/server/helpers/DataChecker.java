af4fa2a59092c1f052f62f1aeaddadf79e83f66b
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-20 18:26:00 +0300

    Update DataChecker.java

--- Lab7/server/helpers/DataChecker.java
+++ Lab7/server/helpers/DataChecker.java
@@ -2,9 +2,12 @@ package helpers;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
+/**
+ * @author Sabitov Danil
+ * @version 1.0
+ * Class describing date and time
+ */
 public class DataChecker {
-
     public static boolean isValidDate(String inDate) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         dateFormat.setLenient(false);
