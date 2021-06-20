b811bc3ffb34e7c7b08a1e13944acd5b2fd17b93
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-20 18:25:40 +0300

    Update Login.java

--- Lab7/client/commands/Login.java
+++ Lab7/client/commands/Login.java
@@ -2,9 +2,15 @@ package commands;
 
 import data.Organization;
 import helpers.User;
-
+/**
+ * Class for command "sign_up" and "sign_in"
+ * @author Sabitov Danil
+ * @version 1.0
+ */
 public class Login {
-
+    /**
+     * Method for realizing "sign_up" command
+     */
     public void sign_up(String login, String password) throws  InterruptedException {
         ClientTCP sender = new ClientTCP();
         sender.setCommand("sign_up");
@@ -14,6 +20,9 @@ public class Login {
         sender.sending(sender);
     }
 
+    /**
+     * Method for realizing "sign_in" command
+     */
     public void sign_in(String login, String password) throws  InterruptedException {
         ClientTCP sender = new ClientTCP();
         sender.setCommand("sign_in");
