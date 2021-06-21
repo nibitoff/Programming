0790b0d68d8c47b1ce944df8cdcdce2a07029b49
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-21 13:50:08 +0300

    Update Remove_first.java

--- Lab7/client/commands/Remove_first.java
+++ Lab7/client/commands/Remove_first.java
@@ -11,8 +11,12 @@ public class Remove_first {
      * Method for realizing this command
      */
     public void remove_first() throws  InterruptedException{
+
+        Organization newOrg = new Organization(null, null, null, "0",
+                null, null, null, null, ClientTCP.myUserID);
         ClientTCP sender = new ClientTCP();
         sender.setCommand("remove_first");
+        sender.setOrg(newOrg);
         sender.sending(sender);
     }
 }
