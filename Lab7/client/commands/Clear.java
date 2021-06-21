4a748997a94ffaa2c5418f72a418f7272e33734b
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-21 13:50:04 +0300

    Update Clear.java

--- Lab7/client/commands/Clear.java
+++ Lab7/client/commands/Clear.java
@@ -1,6 +1,7 @@
 package commands;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import data.Organization;
 
 import java.io.IOException;
 /**
@@ -13,9 +14,11 @@ public class Clear {
      * Method for realizing this command
      */
     public void clear() throws  InterruptedException {
-
+        Organization newOrg = new Organization(null, null, null, "0",
+                null, null, null, null, ClientTCP.myUserID);
         ClientTCP sender = new ClientTCP();
         sender.setCommand("clear");
+        sender.setOrg(newOrg);
         sender.sending(sender);
 
     }
