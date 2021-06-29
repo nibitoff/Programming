90b536772db46dad3955f92a6c5b07d507c4ce12
nibibee <72893410+nibibee@users.noreply.github.com>
2021-06-30 01:14:52 +0300

    Update Server.java

--- Lab7/server/Server.java
+++ Lab7/server/Server.java
@@ -2,28 +2,33 @@ import data.Organization;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 import helpers.*;
 public class Server {
     public static String myResult;
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
 
     public static void main(String[] args) throws InterruptedException, IOException, ClassNotFoundException {
         try {
-            System.out.println("Program started working!");
+            LogManager.getLogManager().readConfiguration(Server.class.getResourceAsStream("logging.properties"));
+            logger.log(Level.INFO,"Program started working!");
             CommandManager commandManager = new CommandManager(new CollectionChecker());
-            System.out.println("Server started!");
+            logger.log(Level.INFO,"Server started!");
         } catch (ArrayIndexOutOfBoundsException exception) {
-            System.out.println("Incorrect filepath was entered! Try again!");
+            logger.log(Level.SEVERE,"Something wrong with Server.class");
             System.exit(1);
         }
         Reciever reciever = new Reciever();
         reciever.checkInput();
         reciever.run();
-
-
     }
 }
 
